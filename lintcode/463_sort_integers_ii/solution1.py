c98d83b8b453722a4136b35548177ee1fe675df8
Kaitian Xie <xkaitian@gmail.com>
2019-11-02 18:20:09 -0700

    464: Solve problem using quick sort

--- lintcode/463_sort_integers_ii/solution1.py
+++ lintcode/463_sort_integers_ii/solution1.py
@@ -5,4 +5,26 @@ class Solution:
     """
 
     def sortIntegers2(self, A):
-        pass
+        self.quick_sort(A, 0, len(A) - 1)
+
+    def quick_sort(self, A, start, end):
+        if start >= end:
+            return
+
+        left = start
+        right = end
+        pivot = A[(start + end) // 2]
+
+        while left <= right:
+            while left <= right and A[left] < pivot:
+                left += 1
+            while left <= right and A[right] > pivot:
+                right -= 1
+
+            if left <= right:
+                A[left], A[right] = A[right], A[left]
+                left += 1
+                right -= 1
+
+        self.quick_sort(A, start, right)
+        self.quick_sort(A, left, end)
