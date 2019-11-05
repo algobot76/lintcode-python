4fb152b7314df8ee5068eac7ec88e1a80d3d2f1c
Kaitian Xie <xkaitian@gmail.com>
2019-11-04 19:52:00 -0800

    31: Begin problem

---
+++ lintcode/31_partition_array/solution1.py
@@ -0,0 +1,9 @@
+class Solution:
+    """
+    @param nums: The integer array you should partition
+    @param k: An integer
+    @return: The index after partition
+    """
+
+    def partitionArray(self, nums, k):
+        pass
