04fa4e30f460abee7f5502aa34bd21481b352969
Kaitian Xie <xkaitian@gmail.com>
2019-11-11 19:55:36 -0800

    544: Begin problem

---
+++ lintcode/544_top_k_largest_numbers/solution1.py
@@ -0,0 +1,9 @@
+class Solution:
+    """
+    @param nums: an integer array
+    @param k: An integer
+    @return: the top k largest numbers in array
+    """
+
+    def topk(self, nums, k):
+        pass
