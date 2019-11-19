18788430a3ed0d10fc48bbffbd67e69d9e317ea1
Kaitian Xie <xkaitian@gmail.com>
2019-11-18 19:08:48 -0800

    533: Begin problem

---
+++ lintcode/533_two_sum_closest_to_target/solution1.py
@@ -0,0 +1,9 @@
+class Solution:
+    """
+    @param nums: an integer array
+    @param target: An integer
+    @return: the difference between the sum and the target
+    """
+
+    def twoSumClosest(self, nums, target):
+        pass
