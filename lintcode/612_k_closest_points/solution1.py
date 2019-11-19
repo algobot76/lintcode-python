3df1531c39ef4279d5917c02907f51facdc6cd6c
Kaitian Xie <xkaitian@gmail.com>
2019-11-19 06:54:53 -0800

    612: Begin problem

---
+++ lintcode/612_k_closest_points/solution1.py
@@ -0,0 +1,19 @@
+"""
+Definition for a point.
+class Point:
+    def __init__(self, a=0, b=0):
+        self.x = a
+        self.y = b
+"""
+
+
+class Solution:
+    """
+    @param points: a list of points
+    @param origin: a point
+    @param k: An integer
+    @return: the k closest points
+    """
+
+    def kClosest(self, points, origin, k):
+        pass
