85a8efd54ae9001bf02dc2839255954c1861ba8d
Kaitian Xie <xkaitian@gmail.com>
2019-11-19 07:03:37 -0800

    612: Solve problem using priority queue

--- lintcode/612_k_closest_points/solution1.py
+++ lintcode/612_k_closest_points/solution1.py
@@ -1,3 +1,7 @@
+import heapq
+
+from lintcode.utils import Point
+
 """
 Definition for a point.
 class Point:
@@ -16,4 +20,19 @@ class Solution:
     """
 
     def kClosest(self, points, origin, k):
-        pass
+        heap = []
+        for point in points:
+            dist = self.get_distance(point, origin)
+            heapq.heappush(heap, (-dist, -point.x, -point.y))
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        ans = []
+        while len(heap) > 0:
+            _, x, y = heapq.heappop(heap)
+            ans.append(Point(-x, -y))
+
+        return ans
+
+    def get_distance(self, point, origin):
+        return (point.x - origin.x) ** 2 + (point.y - origin.y) ** 2
