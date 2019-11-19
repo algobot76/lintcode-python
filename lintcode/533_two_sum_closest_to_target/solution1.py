fcaff38972b0441c76a59856d3a148dc7b469483
Kaitian Xie <xkaitian@gmail.com>
2019-11-18 19:16:59 -0800

    533: Solve problem using two pointers

--- lintcode/533_two_sum_closest_to_target/solution1.py
+++ lintcode/533_two_sum_closest_to_target/solution1.py
@@ -1,3 +1,6 @@
+import sys
+
+
 class Solution:
     """
     @param nums: an integer array
@@ -6,4 +9,17 @@ class Solution:
     """
 
     def twoSumClosest(self, nums, target):
-        pass
+        nums.sort()
+        left = 0
+        right = len(nums) - 1
+
+        diff = sys.maxsize
+        while left < right:
+            sum_ = nums[left] + nums[right]
+            if sum_ > target:
+                right -= 1
+            else:
+                left += 1
+            diff = min(diff, abs(sum_ - target))
+
+        return diff
